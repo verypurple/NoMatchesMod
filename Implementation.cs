be5fda9365bb7771dd09d71dda0403c5eb9a391b
verypurple <61333357+verypurple@users.noreply.github.com>
2021-11-26 14:46:27 +0100

    Bugfix

--- Implementation.cs
+++ Implementation.cs
@@ -1,5 +1,6 @@
 ﻿using MelonLoader;
 using UnityEngine;
+using System.Linq;
 
 namespace NoMatchesMod
 {
@@ -20,22 +21,20 @@ namespace NoMatchesMod
 
         internal static void UpdateGearInScene()
         {
-            var roots = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
+            var items = UnityEngine.SceneManagement.SceneManager
+                .GetAllScenes()
+                .SelectMany(s => s.GetRootGameObjects())
+                .SelectMany(r => r.GetComponentsInChildren<GearItem>(true));
 
-            foreach (var root in roots)
+            foreach (var item in items)
             {
-                var items = root.GetComponentsInChildren<GearItem>(true);
-                
-                foreach (var item in items)
+                bool isMatch = GearFilter.IsMatch(item.name);
+
+                item.m_NonInteractive = isMatch;
+
+                foreach (var renderer in item.GetComponentsInChildren<MeshRenderer>(true))
                 {
-                    bool isMatch = GearFilter.IsMatch(item.name);
-
-                    item.m_NonInteractive = isMatch;
-
-                    foreach (var renderer in item.GetComponentsInChildren<MeshRenderer>(true))
-                    {
-                        renderer.forceRenderingOff = isMatch;
-                    }
+                    renderer.forceRenderingOff = isMatch;
                 }
             }
         }
